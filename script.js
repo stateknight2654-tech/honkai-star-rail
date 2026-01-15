02dae2583ceef48bcc73a28ac52d4295032f4b61
stateknight2654-tech <stateknight2654@gmail.com>
2026-01-15 09:18:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -43,13 +43,3 @@ window.addEventListener("scroll", () => {
     }
   }
 });
-const reveals = document.querySelectorAll(".reveal");
-
-window.addEventListener("scroll", () => {
-  reveals.forEach(el => {
-    const top = el.getBoundingClientRect().top;
-    if (top < window.innerHeight - 100) {
-      el.classList.add("visible");
-    }
-  });
-});
