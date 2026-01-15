0aab7601fb6c6852d3040f38a5d3bb8b06af890b
stateknight2654-tech <stateknight2654@gmail.com>
2026-01-15 10:16:50 +0700

    Update character.js

--- character.js
+++ character.js
@@ -1,10 +1,15 @@
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 
-if (characters[id]) {
-  document.getElementById("name").textContent = characters[id].name;
-  document.getElementById("image").src = characters[id].image;
-  document.getElementById("desc").textContent = characters[id].desc;
+if (characterData[id]) {
+  const data = characterData[id];
+
+  document.getElementById("name").textContent = data.name;
+  document.getElementById("desc").textContent =
+    `Vận mệnh: ${data.path} | Thuộc tính: ${data.element}`;
+
+  document.getElementById("image").src =
+    `anh/characters/${id}.webp`;
 } else {
   document.body.innerHTML = "<h1>Nhân vật không tồn tại</h1>";
 }
