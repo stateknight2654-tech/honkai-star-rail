50e8552dca0370ed3f1e0d392994383c2dc41604
stateknight2654-tech <stateknight2654@gmail.com>
2026-01-15 07:29:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,37 @@ function scrollToIntro() {
         behavior: "smooth"
     });
 }
+/* Scroll reveal */
+window.addEventListener("scroll", () => {
+  document.querySelectorAll(".reveal").forEach(el => {
+    if (el.getBoundingClientRect().top < window.innerHeight * 0.9) {
+      el.classList.add("visible");
+    }
+  });
+});
+
+/* ===== SAO BĂNG ===== */
+
+const meteorLayer = document.getElementById("meteor-layer");
+const featureSection = document.getElementById("features");
+let meteorInterval = null;
+
+function createMeteor() {
+  const meteor = document.createElement("div");
+  meteor.className = "meteor";
+
+  meteor.style.left = Math.random() * window.innerWidth + "px";
+  meteor.style.animationDuration = 1.5 + Math.random() * 1.5 + "s";
+
+  meteorLayer.appendChild(meteor);
+
+  setTimeout(() => meteor.remove(), 3000);
+}
+
+window.addEventListener("scroll", () => {
+  const featureTop = featureSection.getBoundingClientRect().top;
+
+  if (featureTop < window.innerHeight && !meteorInterval) {
+    meteorInterval = setInterval(createMeteor, 500);
+  }
+});
